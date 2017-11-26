feea375e575a05defe8e2419dba872f86ae59421
athousandleaves <superamare@gmail.com>
2017-11-26 13:11:53 -0600

    handle scripts in admin

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
+import Script from 'react-load-script';
 
 class AdminPage extends Component {
+  handleScriptLoad() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+    window.netlifyIdentity.init();
+  }
+
   render() {
     return (
       <div className="admin">
+        <Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js" 
+          onLoad={this.handleScriptLoad.bind(this)}
+        />
         <Helmet>
           <meta charSet="utf-8" />
           <meta
