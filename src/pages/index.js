271c18d52ee09d613e6e9a192de38e6ba184ef88
athousandleaves <superamare@gmail.com>
2017-11-26 11:48:56 -0600

    update with changes to admin

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,21 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
+    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     <h1>Brooke Snellings</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <script>
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        })
+      }
+    </script>
   </div>
 )
 
