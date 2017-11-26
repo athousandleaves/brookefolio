b27940e21edbc08765c5146c45dff8e4290b62a6
athousandleaves <superamare@gmail.com>
2017-11-26 11:24:22 -0600

    add admin component

---
+++ src/pages/admin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Admin = () => (
+  <div>
+  <link rel="stylesheet" href="https://unpkg.com/netlify-cms@^0.7.0/dist/cms.css" />
+  <script src="https://unpkg.com/netlify-cms@^0.7.0/dist/cms.js"></script>
+  </div>
+)
+
+export default Admin
