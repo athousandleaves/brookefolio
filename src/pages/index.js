cac0214e042f2166d80082cc25064e59584138e1
athousandleaves <superamare@gmail.com>
2017-11-26 12:19:33 -0600

    remove extemporaneous script tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,17 +25,6 @@ export default class IndexPage extends React.Component {
         <h1>Brooke Snellings</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
-        <script>
-          if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-              if (!user) {
-                window.netlifyIdentity.on("login", () => {
-                  document.location.href = "/admin/";
-                });
-              }
-            })
-          }
-        </script>
       </div>
     )
   }
