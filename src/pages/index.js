6b752e07d7565751bf8a0545642a89770863a6f6
athousandleaves <superamare@gmail.com>
2017-11-26 11:58:46 -0600

    add script injection for CMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,42 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Script from 'react-load-script'
 
-const IndexPage = () => (
-  <div>
-    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-    <h1>Brooke Snellings</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <script>
-      if (window.netlifyIdentity) {
-        window.netlifyIdentity.on("init", user => {
-          if (!user) {
-            window.netlifyIdentity.on("login", () => {
-              document.location.href = "/admin/";
-            });
+export default class IndexPage extends React.Component {
+  handleScriptLoad() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+    window.netlifyIdentity.init();
+  }
+  
+  render () {
+    return (
+      <div>
+      <Script
+        url="https://identity.netlify.com/v1/netlify-identity-widget.js" 
+        onLoad={this.handleScriptLoad.bind(this)} />
+        <h1>Brooke Snellings</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <script>
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
+                });
+              }
+            })
           }
-        })
-      }
-    </script>
-  </div>
-)
-
-export default IndexPage
+        </script>
+      </div>
+    )
+  }
+}
